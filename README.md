d94b5a5badc0f54b3ff571524183452bdf132554
Geoffrey Abbott <gabbottron@gmail.com>
2022-09-25 13:22:40 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # s3-upload-action
 This is a simple S3 upload action for github
+
+It is a simplified version of: https://github.com/bshelling/s3-jekyll-upload-action
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+cd _site
+echo "-------- Configuration start -----------"
+aws configure set aws_access_key_id $1
+aws configure set aws_secret_access_key $2
+aws configure set region $3
+echo "-------- Configuration Complete -----------"
+echo " "
+echo "Removing assets....."
+aws s3 rm $5 --recursive
+echo "Uploading....."
+aws s3 cp $4 $5 --exclude ".github/*" --exclude ".git/*" $6
+echo "Upload complete"
+# TODO: Add the appropriate permissions for this and re-implement
+#echo "Clearing cache...."
+#aws cloudfront create-invalidation --distribution-id $7 --paths "/*"
+#echo "Cache cleared......"
